0dd5881781f3b825682a422e8fb7c81514ab1ead
daviresende03 <daviresendedev@gmail.com>
2022-09-04 10:32:23 -0300

    Inserindo dados no banco via API

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Navbar />
       <Container customClass="min-height">
         <Routes>
-          <Route exact path="/" element={<Home/>} />
+          <Route path="/" element={<Home/>} />
           <Route path="/company" element={<Company/>} />
           <Route path="/contact" element={<Contact/>} />
           <Route path="/projects" element={<Projects/>} />
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,36 @@
+import {useNavigate} from 'react-router-dom'
 import styles from './NewProject.module.css'
 import ProjectForm from '../project/ProjectForm'
 
 function NewProject() {
+
+    const navigate = useNavigate()
+
+    function CreatePost(project) {
+        //Inicializar o custo e serviços
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            navigate('/projects', {message: 'Projeto Criado com Sucesso!'}) //Redirecionar
+        })
+        .catch((err) => console.log(err))
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto"/>
+            <ProjectForm handleSubmit={CreatePost} btnText="Criar Projeto"/>
         </div>
     )
 }
