13fbd156072ce1889c5ded86470fd505bf9cf47e
daviresende03 <daviresendedev@gmail.com>
2022-09-11 10:39:32 -0300

    Criando a área de serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -13,6 +13,7 @@ function Project() {
     const [showProjectForm, setShowProjectForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
+    const [showServiceForm,setServiceForm] = useState(false)
 
     useEffect(() => {
         setTimeout(() => {
@@ -31,6 +32,7 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
         if(project.budget < project.cost){
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setType('error')
@@ -56,6 +58,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setServiceForm(!showServiceForm)
     }
 
     return (
@@ -90,6 +96,23 @@ function Project() {
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm && (
+                                    <div>
+                                        Formulário do Serviço
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass="start">
+                            <p>Itens de Serviços</p>
+                        </Container>
                     </Container>
                 </div>
             ): (
